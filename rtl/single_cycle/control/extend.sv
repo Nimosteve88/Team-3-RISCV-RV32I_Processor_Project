3e4fafc912e18e5374f2e2fd655fe2d79ee5294e
sne-samal <sne.samal@gmail.com>
2023-11-30 09:57:02 +0000

    Update extend.sv
    Updated sign extend for more instruction types.

--- rtl/single_cycle/control/extend.sv
+++ rtl/single_cycle/control/extend.sv
@@ -1,12 +1,18 @@
 module signextend #(
     parameter WIDTH = 32
 )(
-    input logic              Immsrc,
+    input logic [1:0]        Immsrc,
     input logic [WIDTH-1:0]  instr,
     output logic [WIDTH-1:0] Immop
 );
 
-    assign Immop = Immsrc ? {{20{instr[31]}}, instr[31:20]} : {{20{instr[31]}}, instr[7], instr[30:25], instr[11:8], 1'b0};
-    
+    //assign Immop = Immsrc ? {{20{instr[31]}}, instr[31:20]} : {{20{instr[31]}}, instr[7], instr[30:25], instr[11:8], 1'b0};
+always_comb
+    case (Immsrc)
+        2'b00:  Immop = {{20{instr[31]}}, instr[31:20]};                                    // I instruction
+        2'b01:  Immop = {{20{instr[31]}}, instr[31:25], instr[11:7]};                       // S Instruction
+        2'b10:  Immop = {{20{instr[31]}}, instr[7], instr[30:25], instr[11:8], 1’b0};       // B Instruction
+        2'b11:  ImmOp = {{12{instr[31]}}, instr[19:12], instr[20], instr[30:21], 1’b0};     // J Instruction
+    endcase
 
 endmodule

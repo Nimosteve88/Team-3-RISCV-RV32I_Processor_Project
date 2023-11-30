03e3f32f6a40df808f68a0376a74493198982c73
Nimosteve88 <nimosteve2@gmail.com>
2023-11-30 16:51:49 +0000

    Alu update

--- rtl/single_cycle/data/alu.sv
+++ rtl/single_cycle/data/alu.sv
@@ -6,7 +6,7 @@ module alu #(
     input   logic [WIDTH-1:0]     SrcB,
     input   logic [2:0]           ALUctrl,
     output  logic [WIDTH-1:0]     ALUResult,
-    output  logic                 EQ
+    output  logic                 Zero
 );
 
     // changed from 32'd255 to take that operand as t1, so that bnez also works
@@ -16,17 +16,17 @@ always_comb begin
       3'b000: begin
         // ALU control is 3'b000, so perform addition
         ALUResult <= SrcA + SrcB;
-        EQ <= EQ; // Set EQ to 0 (not used in this case)
+        Zero <= Zero; // Set Zero to 0 (not used in this case)
       end
       3'b001: begin
         // ALU control is 3'b001, so check for equality
-        EQ <= (ALUop1 == ALUop2) ? 1 : 0;
+        Zero <= (ALUop1 == ALUop2) ? 1 : 0;
         SUM <= 0; // Set SUM to 0 (not used in this case)
       end
       default: begin
         // Handle other cases if needed
         ALUResult <= 0;
-        EQ <= 0;
+        Zero <= 0;
       end
     endcase
   end

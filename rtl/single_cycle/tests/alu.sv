47b9fb0b87b971493608b862f7f71f758dcc665b
sne-samal <sne.samal@gmail.com>
2023-12-04 12:51:00 +0000

    Added ALU test files

--- rtl/single_cycle/tests/alu.sv
+++ rtl/single_cycle/tests/alu.sv
@@ -4,7 +4,7 @@ module alu #(
 )(
     input   logic [WIDTH-1:0]     SrcA,
     input   logic [WIDTH-1:0]     SrcB,
-    input   logic [2:0]           ALUctrl,
+    input   logic [3:0]           ALUctrl,
     output  logic [WIDTH-1:0]     ALUResult,
     output  logic                 Zero
 );
@@ -14,14 +14,19 @@ module alu #(
 always_comb begin
     case (ALUctrl)
       3'b000: begin
-        // ALU control is 3'b000, so perform addition
+        // ALU control is 3'b000, so perform addition (add)
         ALUResult <= SrcA + SrcB;
-        Zero <= Zero; // Set Zero to 0 (not used in this case)
+        Zero <= (SrcA == SrcB) ? 1 : 0; // Set Zero to 0 (not used in this case)
       end
       3'b001: begin
-        // ALU control is 3'b001, so check for equality
-        Zero <= (SrcA == SrcB) ? 1 : 0;
-        ALUResult <= 0; // Set SUM to 0 (not used in this case)
+        // ALU control is 3'b001, so check for equality  (sub)
+        ALUResult <= SrcA - SrcB;
+        Zero <= (SrcA == SrcB) ? 1 : 0; // Set Zero to 0 (not used in this case)
+      end
+      3'b100: begin
+        // ALU control is 3'b010, so check for (xor))
+        ALUResult <= SrcA ^ SrcB;
+        Zero <= (SrcA == SrcB) ? 1 : 0; // Set Zero to 0 (not used in this case)
       end
       default: begin
         // Handle other cases if needed

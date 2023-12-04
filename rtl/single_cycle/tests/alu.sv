2e19c9ccd943e6e467ef013a403b6887396a4c51
sne-samal <sne.samal@gmail.com>
2023-12-04 19:06:10 +0000

    Update alu.sv
    Changed signal name to be more consistent

--- rtl/single_cycle/tests/alu.sv
+++ rtl/single_cycle/tests/alu.sv
@@ -5,7 +5,7 @@ module alu #(
     input   logic [WIDTH-1:0]     SrcB,
     input   logic [2:0]           ALUctrl,
     output  logic [WIDTH-1:0]     ALUResult,
-    output  logic                 Zero
+    output  logic                 EQ
 );
 
     // changed from 32'd255 to take that operand as t1, so that bnez also works
@@ -15,27 +15,27 @@ always_comb begin
       3'b000: begin
         // ALU control is 3'b000, so perform addition (add)
         ALUResult <= SrcA + SrcB;
-        Zero <= (SrcA == SrcB) ? 1 : 0; 
+        EQ <= (SrcA == SrcB) ? 1 : 0; 
       end
       3'b001: begin
         // ALU control is 3'b001, so check for equality  (sub)
         ALUResult <= SrcA - SrcB;
-        Zero <= (SrcA == SrcB) ? 1 : 0; 
+        EQ <= (SrcA == SrcB) ? 1 : 0; 
       end
       3'b010: begin
         // AND
         ALUResult <= SrcA & SrcB;
-        Zero <= (SrcA == SrcB) ? 1 : 0;
+        EQ <= (SrcA == SrcB) ? 1 : 0;
       end 
       3'b100: begin
         // ALU control is 3'b100, so check for (xor))
         ALUResult <= SrcA ^ SrcB;
-        Zero <= (SrcA == SrcB) ? 1 : 0; 
+        EQ <= (SrcA == SrcB) ? 1 : 0; 
       end
       default: begin
         // Handle other cases if needed
         ALUResult <= 0;
-        Zero <= 0;
+        EQ <= 0;
       end
     endcase
   end

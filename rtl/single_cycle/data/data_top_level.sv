8a9c7cedf3de2b468dde436529b63297f3c0cd77
sne-samal <sne.samal@gmail.com>
2023-12-04 19:05:20 +0000

    Update data_top_level.sv
    Modified signal names and changed some to be outputs or inputs as they are needed elsewhere or are now input signals

--- rtl/single_cycle/data/data_top_level.sv
+++ rtl/single_cycle/data/data_top_level.sv
@@ -7,29 +7,25 @@ module data_top_level #(
     input logic  [REGWIDTH-1:0]     rs2,
     input logic  [REGWIDTH-1:0]     rd,
     input logic                     RegWrite,
-    //input logic  [DATAWIDTH-1:0]    ALUop1,
-    //input logic  [DATAWIDTH-1:0]    regOp2,
-    //input logic  [DATAWIDTH-1:0]    ALUop2,
     input logic                     ALUsrc,
     input logic  [2:0]              ALUctrl,
     input logic  [DATAWIDTH-1:0]    ImmOp,
-    //output logic [DATAWIDTH-1:0]    ALUout,
+    input logic  [DATAWIDTH-1:0]    writeData,
     output logic                    EQ,
     output logic [DATAWIDTH-1:0]    a0,
-    output logic [DATAWIDTH-1:0]    ALUop1 // used exclusively for jalr
+    output logic [DATAWIDTH-1:0]    ALUop1, // used exclusively for jalr
+    output logic [DATAWIDTH-1:0]    ALUout,
+    output logic [DATAWIDTH-1:0]    regOp2
 );
 
-// logic [DATAWIDTH-1:0]               ALUop1;
-logic [DATAWIDTH-1:0]               regOp2;
 logic [DATAWIDTH-1:0]               ALUop2;
-logic [DATAWIDTH-1:0]               ALUout;
 
 regfile register(
     .AD1(rs1),
     .AD2(rs2),
     .AD3(rd),
     .WE3(RegWrite),
-    .WD3(ALUout),
+    .WD3(writeData),
     .clk(clk),
     .RD1(ALUop1),
     .RD2(regOp2),

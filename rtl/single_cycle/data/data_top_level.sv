b285a48691f625407a420de97ca7553f6c29e2f7
sne-samal <sne.samal@gmail.com>
2023-12-05 22:04:26 +0000

    Modified for F1 program

--- rtl/single_cycle/data/data_top_level.sv
+++ rtl/single_cycle/data/data_top_level.sv
@@ -15,12 +15,15 @@ module data_top_level #(
     output logic [DATAWIDTH-1:0]    a0,
     output logic [DATAWIDTH-1:0]    ALUop1, // used exclusively for jalr
     output logic [DATAWIDTH-1:0]    ALUout,
-    output logic [DATAWIDTH-1:0]    regOp2
+    output logic [DATAWIDTH-1:0]    regOp2,
+
+    input logic  [DATAWIDTH-1:0]    t4,     // F1 program inputs
+    input logic                     t0      // F1 program inputs
 );
 
 logic [DATAWIDTH-1:0]               ALUop2;
 
-regfile register(
+register_file register(
     .AD1(rs1),
     .AD2(rs2),
     .AD3(rd),
@@ -29,7 +32,9 @@ regfile register(
     .clk(clk),
     .RD1(ALUop1),
     .RD2(regOp2),
-    .a0(a0)
+    .a0(a0),
+    .t4(t4),    // F1 program inputs
+    .t0(t0)     // F1 program inputs
 );
 
 alu_src_mux multi(
@@ -40,10 +45,10 @@ alu_src_mux multi(
 );
 
 alu ALU(
-    .ALUop1 (ALUop1),
-    .ALUop2 (ALUop2),
+    .SrcA (ALUop1),
+    .SrcB (ALUop2),
     .ALUctrl (ALUctrl),
-    .SUM (ALUout),
+    .ALUResult (ALUout),
     .EQ (EQ)
 );
 

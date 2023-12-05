37433f56447bae5c195b346bd537be5d4ff6685e
sne-samal <sne.samal@gmail.com>
2023-12-05 12:40:19 +0000

    Update alu.sv
    Added debugged alu

--- rtl/single_cycle/data/alu.sv
+++ rtl/single_cycle/data/alu.sv
@@ -1,12 +1,11 @@
 module alu #(
     parameter WIDTH = 32,
-    parameter SH_WIDTH = 5
 )(
     input   logic [WIDTH-1:0]     SrcA,
     input   logic [WIDTH-1:0]     SrcB,
     input   logic [2:0]           ALUctrl,
     output  logic [WIDTH-1:0]     ALUResult,
-    output  logic                 Zero
+    output  logic                 EQ
 );
 
     // changed from 32'd255 to take that operand as t1, so that bnez also works
@@ -16,47 +15,29 @@ always_comb begin
       3'b000: begin
         // ALU control is 3'b000, so perform addition (add)
         ALUResult <= SrcA + SrcB;
-        Zero <= Zero; // Set Zero to 0 (not used in this case)
+        EQ <= (SrcA == SrcB) ? 1 : 0; 
       end
       3'b001: begin
         // ALU control is 3'b001, so check for equality  (sub)
         ALUResult <= SrcA - SrcB;
-        Zero <= Zero; // Set Zero to 0 (not used in this case)
+        EQ <= (SrcA == SrcB) ? 1 : 0; 
       end
       3'b010: begin
-        // ALU control is 3'b010, so check for (xor))
-        ALUResult <= SrcA ^ SrcB;
-        Zero <= Zero; // Set Zero to 0 (not used in this case)
-      end
-      3'b011: begin
-        // ALU control is 3'b011, so check for (and)
+        // AND
         ALUResult <= SrcA & SrcB;
-        Zero <= Zero; // Set Zero to 0 (not used in this case)
-      end
+        EQ <= (SrcA == SrcB) ? 1 : 0;
+      end 
       3'b100: begin
-        // ALU control is 3'b100, so check for (jal)
-        ALUResult <= SrcB;
-        Zero <= Zero; // Set Zero to 0 (not used in this case)
-      end
-      3'b101: begin
-        // ALU control is 3'b101, so check for equality (bne)
-        Zero <= (ALUop1 == ALUop2) ? 1 : 0;
-        ALUResult <= 0; // Set SUM to 0 (not used in this case)
-      end
-      3'b0111: begin
-        // ALU control is 3'b111, so check for equality (beq)
-        Zero <= (ALUop1 != ALUop2) ? 1 : 0;
-        ALUResult <= 0; // Set SUM to 0 (not used in this case)
+        // ALU control is 3'b100, so check for (xor))
+        ALUResult <= SrcA ^ SrcB;
+        EQ <= (SrcA == SrcB) ? 1 : 0; 
       end
       default: begin
         // Handle other cases if needed
         ALUResult <= 0;
-        Zero <= 0;
+        EQ <= 0;
       end
     endcase
   end
      
 endmodule
-
-
-//operations to consider:  bne, beq, add, and, xor,  sub, jal

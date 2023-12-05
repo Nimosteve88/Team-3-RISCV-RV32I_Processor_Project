297cc8a73d5ead0d9fc9aad680b3f38b45585017
sne-samal <sne.samal@gmail.com>
2023-12-05 12:39:58 +0000

    Update control_unit.sv
    Added debugged control unit

--- rtl/single_cycle/control/control_unit.sv
+++ rtl/single_cycle/control/control_unit.sv
@@ -1,13 +1,14 @@
-module controlunit (
+module control_unit (
     input logic                 EQ,
     input logic [31:0]          instr,
-    output logic                PCSrc,
+    output logic [1:0]          PCSrc,
     output logic [1:0]          ResultSrc,
     output logic                MemWrite,
     output logic [2:0]          ALUctrl, // changed from [3:0]
     output logic                ALUsrc,
     output logic [2:0]          ImmSrc,
-    output logic                RegWrite
+    output logic                RegWrite,
+    output logic                ByteAddr // added
 );
     logic [6:0] opcode = instr[6:0];
     logic        funct7 = instr[30]; //only 5th bit of funct7 is used which is bit 30 of instr word
@@ -15,22 +16,25 @@ module controlunit (
 
 always_comb begin
     if (opcode == 7'b1100011) // B-type instruction - bne and beq
+    begin
         case (funct3)
             3'b000: begin  // beq instruction
-                ALUctrl <= 3'b111;
+                PCSrc <= EQ ? 2'b01 : 2'b00;
             end
             3'b001: begin // bne instruction
-                ALUctrl <= 3'b101;
+                PCSrc <= EQ ? 2'b00 : 2'b01;
             end
         endcase
         begin
             ALUsrc   <= 0;
             RegWrite <= 0;
-            PCSrc <= ~(EQ);
             ImmSrc <= 3'b010;
+            ALUctrl <= 3'b001;
             MemWrite <= 0;
-            ResultSrc <= 0; //value doesn't matter here as not using ALU result
+            ResultSrc <= 2'b00; //value doesn't matter here as not using ALU result
+            ByteAddr <= 0;
         end
+    end
     
     else if (opcode == 7'b0010011)// I-type instruction - addi instruction
         begin
@@ -38,12 +42,14 @@ always_comb begin
             ALUsrc   <= 1;
             RegWrite <= 1;
             PCSrc    <= 2'b00;
-            ImmSrc <= 1;
+            ImmSrc <= 3'b000; // changed to correct value
             MemWrite <= 0;
             ResultSrc <= 0;
+            ByteAddr <= 0;
         end
 
     else if (opcode == 7'b0110011) // R-type instruction - add, and, xor, sub
+    begin
         case (funct3)
             3'b000: // add or sub
               if (funct7) begin //sub function
@@ -53,10 +59,10 @@ always_comb begin
                 ALUctrl <= 3'b000;
               end
             3'b100: begin // xor function for ALU
-                ALUctrl <= 3'b010; 
+                ALUctrl <= 3'b100; // changed
             end
             3'b111: begin // and function for ALU
-                ALUctrl <= 3'b011; 
+                ALUctrl <= 3'b010; 
             end
             default: begin
                 ALUctrl <= 3'b000;
@@ -65,32 +71,36 @@ always_comb begin
         begin
             RegWrite <= 1;
             ALUsrc <= 0;
-            ImmSrc <= 3'b??;
+            ImmSrc <= 3'b000;
             PCSrc <= 2'b00;
             ResultSrc <= 0;
             MemWrite <= 0;
+            ByteAddr <= 0;
         end
+    end
     
     else if (opcode == 7'b1101111) // J-type instruction - jal (fixed opcode for j-type)
         begin
             RegWrite <= 1;
-            ALUsrc <= 1'b?;
-            ALUctrl <= 3'b???;
+            ALUsrc <= 1'b0;
+            ALUctrl <= 3'b000;
             ImmSrc <= 3'b011;
             MemWrite <= 0;
             ResultSrc <= 2'b10;
             PCSrc <= 2'b01;
+            ByteAddr <= 0;
         end
     
     else if (opcode == 7'b1100111) // JALR instruction
         begin
-            RegWrite <= 0;
+            RegWrite <= 1;
             ALUsrc <= 1'b1;
             ALUctrl <= 3'b000; 
             ImmSrc <= 3'b000; // JALR is I-type
             MemWrite <= 0;
             ResultSrc <= 2'b10;
             PCSrc <= 2'b10; // Made PCsrc 2 bit to allow for jalr
+            ByteAddr <= 0; 
         end
     
     else if (opcode == 7'b0110111) // U-type instruction - LUI
@@ -102,6 +112,7 @@ always_comb begin
             MemWrite <= 0;
             ResultSrc <= 2'b11;
             PCSrc <= 0;
+            ByteAddr <= 0;
         end
 
     else if (opcode == 7'b0100011) // S-type instruction - SB
@@ -111,9 +122,22 @@ always_comb begin
             ALUctrl <= 0;
             ImmSrc <= 3'b001;
             MemWrite <= 1;
-            ResultSrc <= 0; //dont care
+            ResultSrc <= 2'b00; //dont care
             PCSrc <= 0;
+            ByteAddr <= 1;
         end
+    
+    else if  (opcode == 7'b0000011) // LBU instruction - added
+    begin
+        RegWrite <= 1;
+        ALUsrc <= 1;
+        ALUctrl <= 0;
+        ImmSrc <= 3'b000;
+        MemWrite <= 0;
+        ResultSrc <= 2'b01; //dont care
+        PCSrc <= 0;
+        ByteAddr <= 1;
+    end
     
     // EQ = 0;
 end

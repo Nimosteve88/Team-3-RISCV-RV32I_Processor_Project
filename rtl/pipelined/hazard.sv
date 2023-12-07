b294d22c79a16ab46fa55ab911c2734dadb079cb
sne-samal <sne.samal@gmail.com>
2023-12-07 22:10:36 +0000

    Update hazard.sv
    Fixed typos and added missing input

--- rtl/pipelined/hazard.sv
+++ rtl/pipelined/hazard.sv
@@ -10,6 +10,7 @@ module hazard_unit (
 
     // execute
     output  logic               FlushE,
+    input   logic   [4:0]       RdE,
     input   logic   [4:0]       Rs1E,
     input   logic   [4:0]       Rs2E,
     output  logic   [1:0]       ForwardAE,
@@ -35,15 +36,15 @@ always_comb begin
     // Check if the source register (Rs1R) matches the destination register (RdM) in the memory stage (M) 
     // AND if register write is enabled in the memory stage (RegWriteM)
     // AND if the source register is not zero (Rs1E != 0)
-    if ((Rs1R == RdM) && RegWriteM) && (Rs1E != 0) begin
+    if (((Rs1E == RdM) && RegWriteM) && (Rs1E != 0)) begin
         // If all conditions are met, set ForwardAE to 10
-        ForwardAE = 2'10;
+        ForwardAE = 2'b10;
     end
 
     // Check if the source register (Rs1E) matches the destination register (RdW) in the write-back stage (W)
     // AND if register write is enabled in the write-back stage (RegWriteW)
     // AND if the source register is not zero (Rs1E != 0)
-    else if ((Rs1E == RdW) && RegWriteW) && (Rs1E != 0) begin
+    else if (((Rs1E == RdW) && RegWriteW) && (Rs1E != 0)) begin
         // If all conditions are met, set ForwardAE to 01
         ForwardAE = 2'b01;
     end
@@ -54,11 +55,11 @@ always_comb begin
     end
 
     // The same goes for Rs2 and ForwardBE
-     if ((Rs2R == RdM) && RegWriteM) && (Rs2E != 0) begin
-        ForwardBE = 2'10;
+     if (((Rs2E == RdM) && RegWriteM) && (Rs2E != 0)) begin
+        ForwardBE = 2'b10;
     end
 
-    else if ((Rs2E == RdW) && RegWriteW) && (Rs2E != 0) begin
+    else if (((Rs2E == RdW) && RegWriteW) && (Rs2E != 0)) begin
         ForwardBE = 2'b01;
     end
 
@@ -73,18 +74,19 @@ always_comb begin
     // Check if there is a result from the source in the execution stage (ResultSrcE0)
     // AND if either the source register 1 (Rs1D) matches the destination register in the execute stage (RdE)
     // OR if the source register 2 (Rs2D) matches the destination register in the execute stage (RdE)
-    lwStall = (ResultSrcE == 2'b01) && ((Rs1D == RdE) || (Rs2D == RdE));
+    lwStall = ((ResultSrcE == 2'b01) && ((Rs1D == RdE) || (Rs2D == RdE)));
 
     // Set various pipeline stage stall signals based on the load hazard condition
     // If lwStall is true, set StallF, StallD, to true, indicating stalls in Fetch, Decode, and Execute stages
-    StallF = StallD = lwStall;
+    StallF = lwStall;
+    StallD = lwStall;
 
 
 
 
     // ** Flush decode and execute staged when a branch is taken or a load introduces a bubble:
     // 2 cycle stall for lw instructions, avoiding execute for instructions following taken jumps or branches
-    branch_or_jump = (PCSrcE == 2'b01) || (PCSrcE == 2'b10);
+    branch_or_jump = ((PCSrcE == 2'b01) || (PCSrcE == 2'b10));
     FlushD = branch_or_jump;
     FlushE = lwStall || branch_or_jump;
 

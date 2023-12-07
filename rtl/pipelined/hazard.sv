539ff5c85ada67ce0640d704595a0302224fe389
yanniszioulis <124446659+yanniszioulis@users.noreply.github.com>
2023-12-07 11:57:04 +0000

    Added hazard logic

--- rtl/pipelined/hazard.sv
+++ rtl/pipelined/hazard.sv
@@ -22,8 +22,43 @@ module hazard_unit (
 
     // write back
     input   logic   [4:0]       RdW,
-    input   logic               RegWriteW,
+    input   logic               RegWriteW
 );
 
+always_comb begin
+    // ** Data Hazard Forwarding Logic:
+
+    // Check if the source register (Rs1R) matches the destination register (RdM) in the memory stage (M) 
+    // AND if register write is enabled in the memory stage (RegWriteM)
+    // AND if the source register is not zero (Rs1E != 0)
+    if ((Rs1R == RdM) & RegWriteM) & (Rs1E != 0) begin
+        // If all conditions are met, set ForwardAE to 10
+        ForwardAE = 10
+    end
+
+    // Check if the source register (Rs1E) matches the destination register (RdW) in the write-back stage (W)
+    // AND if register write is enabled in the write-back stage (RegWriteW)
+    // AND if the source register is not zero (Rs1E != 0)
+    else if ((Rs1E == RdW) & RegWriteW) & (Rs1E != 0) begin
+        // If all conditions are met, set ForwardAE to 01
+        ForwardAE = 01
+    end
+
+    // If none of the above conditions are met, set ForwardAE to 00
+    else begin
+        ForwardAE = 00
+    end
+
+    // ** Data Hazard Stalls Logic (Occurs when a load hazard takes place):
+
+    // Check if there is a result from the source in the execution stage (ResultSrcE0)
+    // AND if either the source register 1 (Rs1D) matches the destination register in the execute stage (RdE)
+    // OR if the source register 2 (Rs2D) matches the destination register in the execute stage (RdE)
+    lwStall = ResultSrcE0 & ((Rs1D == RdE) | (Rs2D == RdE))
+
+    // Set various pipeline stage stall signals based on the load hazard condition
+    // If lwStall is true, set StallF, StallD, FlushE to true, indicating stalls in Fetch, Decode, and Execute stages
+    StallF = StallD = FlushE = lwStall
+end
 
 endmodule

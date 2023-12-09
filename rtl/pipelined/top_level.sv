5483487574f2b587e114e3e3c9f94c4078dc43e7
sne-samal <sne.samal@gmail.com>
2023-12-09 21:50:54 +0000

    Update top_level.sv
    Setup

--- rtl/pipelined/top_level.sv
+++ rtl/pipelined/top_level.sv
@@ -42,13 +42,22 @@ module top_level #(
 //##################################################################//
 
 fetch program_counter (
-
+    .clk(),
+    .rst(),
+    .en(),
+    .ImmExtE(),
+    .PCSrcE(),
+    .SrcAE(),
+    .PCE(),
+    .PCF(),
+    .PCPlus4F()
 )
 
 
 
 instruction_memory instruction_mem (
-
+    .A(),
+    .RD()
 )
 
 
@@ -60,25 +69,54 @@ instruction_memory instruction_mem (
 //##################################################################//
 
 decode decode_pipeline_register (
-
+    .clk(),
+    .EN(),
+    .CLR(),
+    .InstrDi(),
+    .PCF(),
+    .PC_plus_4F(),
+    .PCD(),
+    .PC_plus_4D(),
+    .InstrDo()
 )
 
 
 
 extend sign_extension_unit (
-
+    .Immsrc(),
+    .instr(),
+    .Immop()
 )
 
 
 
 control_unit_pipelined control_unit (
-
+    .instr(),
+    .ResultSrc(),
+    .MemWrite(),
+    .ALUctrl(),
+    .ALUsrc(),
+    .ImmSrc(),
+    .RegWrite(),
+    .ByteAddr(),
+    .JumpType(),
+    .BranchType()
 )
 
 
 
 register_file registers (
-
+    .AD1(),
+    .AD2(),
+    .AD3(),
+    .WE3(),
+    .WD3(),
+    .clk(),
+    .RD1(),
+    .RD2(),
+    .a0(),
+    .t4(),
+    .t0()
 )
 
 
@@ -90,37 +128,88 @@ register_file registers (
 //##################################################################//
 
 execute execute_pipeline_register (
-
+    .clk(),
+    .CLR(),
+    .RegWriteD(),
+    .ResultSrcD(),
+    .MemWriteD(),
+    .ByteAddrD(),
+    .JumpTypeD(),
+    .BranchTypeD(),
+    .ALUControlD(),
+    .ALUsrcD(),
+    .RD1D(),
+    .RD2D(),
+    .PCD(),
+    .Rs1D(),
+    .Rs2D(),
+    .RdD(),
+    .ImmExtD(),
+    .PCPlus4D(),
+    .RegWriteE(),
+    .ResultSrcE(),
+    .MemWriteE(),
+    .ByteAddrE(),
+    .JumpTypeE(),
+    .BranchTypeE(),
+    .ALUControlE(),
+    .ALUsrcE(),
+    .RD1E(),
+    .RD2E(),
+    .PCE(),
+    .Rs1E(),
+    .Rs2E(),
+    .RdE(),
+    .ImmExtE(),
+    .PCPlus4E()
 )
 
 
 
 rd1emux RD1E_mux (
-
+    .RD1E(),
+    .WD3(),
+    .ALUResult(),
+    .ForwardAE(),
+    .SrcAE()
 )
 
 
 
 rd2emux RD2E_mux (
-
+    .RD2E(),
+    .WD3(),
+    .ALUResult(),
+    .ForwardBE(),
+    .SrcBE()
 )
 
 
 
 alu_src_mux ALU_mux (
-
+    .d0(),
+    .d1(),
+    .ALUsrc(),
+    .y()
 )
 
 
 
 alu ALU (
-
+    .SrcAE(),
+    .SrcBE(),
+    .ALUcontrolE(),
+    .ALUResult(),
+    .EQ()
 )
 
 
 
 PCSrclogic pc_src_logic (
-
+    .EQ(),
+    .JumpTypeE(),
+    .BranchTypeE(),
+    .PCSrcE()
 ) 
 
 
@@ -132,13 +221,36 @@ PCSrclogic pc_src_logic (
 //##################################################################//
 
 memory memory_pipeline_register (
-
+    .clk(),
+    .RegWriteE(),
+    .ResultsrcE(),
+    .MemWriteE(),
+    .ByteAddrE(),
+    .ALUResultE(),
+    .WriteDataE(),
+    .ImmExtE(),
+    .RdE(),
+    .PCPlus4E(),
+    .RegWriteM(),
+    .ResultsrcM(),
+    .MemWriteM(),
+    .ByteAddrM(),
+    .ALUResultM(),
+    .WriteDataM(),
+    .ImmExtM(),
+    .RdM(),
+    .PCPlus4M()
 )
 
 
 
 data_memory data_mem (
-
+    .clk(),
+    .WE(),
+    .A(),
+    .WD(),
+    .ByteAddr(),
+    .RD()
 )
 
 
@@ -150,13 +262,32 @@ data_memory data_mem (
 //##################################################################//
 
 write_back write_back_pipeline_register (
-
+    .clk(),
+    .RegWriteM(),
+    .ResultSrcM(),
+    .ALUResultM(),
+    .ReadDataM(),
+    .ImmExtM(),
+    .RdM(),
+    .PCPlus4M(),
+    .RegWriteW(),
+    .ResultsrcW(),
+    .ALUResultW(),
+    .ReadDataW(),
+    .ImmExtW(),
+    .RdW(),
+    .PCPlus4W()
 )
 
 
 
 result_mux result_data (
-
+    .result_src(),
+    .ALU_result(),
+    .read_data(),
+    .pc_plus_4(),
+    .immediate(),
+    .result()
 )
 
 
@@ -167,7 +298,23 @@ result_mux result_data (
 //##################################################################//
 
 hazard hazard_unit (
-
+    .StallF(),
+    .StallD(),
+    .FlushD(),
+    .Rs1D(),
+    .Rs2D(),
+    .FlushE(),
+    .RdE(),
+    .Rs1E(),
+    .Rs2E(),
+    .ForwardAE(),
+    .ForwardBE(),
+    .ResultSrcE(),
+    .PCSrcE(),
+    .RdM(),
+    .RegWriteM(),
+    .RdW(),
+    .RegWriteW()
 )
 
 

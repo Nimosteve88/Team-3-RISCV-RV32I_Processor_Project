d9553fb9cd258a866c65d64da693e5316d3d2c99
sne-samal <sne.samal@gmail.com>
2023-12-13 13:31:38 +0000

    Update cache.sv
    modifications:
    - removed WData - not used by cache
    - removed read_data_mem - not used
    - changed cache array widths to be 155 - tag is 26 bits
    - Added variables for set number, block offset and tag to make code more readable
    - changed valid bit number to be 154 - from 155 since cache width changed in change 3
    - changed cache tag location to 153:128 - correctly compare tag fields
    - changed byte addr case - byte addr just takes the least significant byte of the 32 bit word and the rest is 0 extended
    - changed WEN_cache to be an internal signal - ~Hit
    - cleaned up foramatting - removed unecessary begin and ends in swtich cases

--- rtl/cache/tests/cache.sv
+++ rtl/cache/tests/cache.sv
@@ -3,30 +3,36 @@ module cache #(
     WIDTH = 8
 ) (
     input logic                             clk,
-    input logic                             WEN_cache,
+    // input logic                             WEN_cache,       changed to internal signal 
     /* verilator lint_off UNUSED */
     input logic     [DATA_WIDTH-1:0]        A,
     /* verilator lint_on UNUSED */
-    input logic     [DATA_WIDTH-1:0]        WData,
+    // input logic     [DATA_WIDTH-1:0]        WData,           Removed - not used in this code 
     output logic                            Hit,
     input logic                             ByteAddr,
     input logic    [DATA_WIDTH-1:0]         d0,                 // added as data outputs to be written to cache for spatial locality
     input logic    [DATA_WIDTH-1:0]         d1,                 // ''
     input logic    [DATA_WIDTH-1:0]         d2,                 // ''
     input logic    [DATA_WIDTH-1:0]         d3,                 // ''      
-    input logic    [DATA_WIDTH-1:0]         read_data_mem,      // changed name for cache implementation.
+    // input logic    [DATA_WIDTH-1:0]         read_data_mem,   not used    // changed name for cache implementation.
     output logic   [DATA_WIDTH-1:0]         dout_cache
 );
 
 
-logic [155:0] cache_array [2'h3:2'b0]; // V, tag (27 bits), 4*32 bit words
+logic [155:0] cache_array [2'h3:2'b0]; // V, tag (26 bits), 4*32 bit words
+
 logic [DATA_WIDTH-1:0]                      pre_dout; 
 
+logic [1:0] block_offset = A[3:2];
+logic [1:0] set_number = A[5:4];
+logic [25:0] tag = A[31:6];
+
+
 initial begin
-    cache_array[0] = 156'b0;
-    cache_array[1] = 156'b0;
-    cache_array[2] = 156'b0;
-    cache_array[3] = 156'b0;
+    cache_array[0] = 155'b0;
+    cache_array[1] = 155'b0;
+    cache_array[2] = 155'b0;
+    cache_array[3] = 155'b0;
 end
 
 
@@ -34,23 +40,15 @@ end
 always_comb
 begin
     if (WEN_cache == 0) begin
-        case (A[5:4])
+        case (set_number)
                 2'b00: begin
-                    if ((A[30:6] == cache_array[0][30:6]) && (cache_array[0][155] == 1)) begin
+                    if ((tag == cache_array[0][153:128]) && (cache_array[0][154] == 1)) begin
                         Hit <= 1;
-                        case (A[3:2])
-                            2'b00: begin
-                                pre_dout <= cache_array[0][31:0];
-                            end
-                            2'b01: begin
-                                pre_dout <= cache_array[0][63:32];
-                            end
-                            2'b10: begin
-                                pre_dout <= cache_array[0][95:64];
-                            end
-                            2'b11: begin
-                                pre_dout <= cache_array[0][127:96];
-                            end
+                        case (block_offset)
+                            2'b00:          pre_dout = cache_array[0][31:0];
+                            2'b01:          pre_dout = cache_array[0][63:32];
+                            2'b10:          pre_dout = cache_array[0][95:64];
+                            2'b11:          pre_dout = cache_array[0][127:96];
                         endcase
                     end
                     else begin
@@ -58,21 +56,13 @@ begin
                     end
                 end
                 2'b01: begin
-                    if ((A[30:6] == cache_array[1][30:6]) && (cache_array[1][155] == 1)) begin
+                    if ((tag == cache_array[1][153:128]) && (cache_array[1][154] == 1)) begin
                         Hit <= 1;
-                        case (A[3:2])
-                            2'b00: begin
-                                pre_dout <= cache_array[1][31:0];
-                            end
-                            2'b01: begin
-                                pre_dout <= cache_array[1][63:32];
-                            end
-                            2'b10: begin
-                                pre_dout <= cache_array[1][95:64];
-                            end
-                            2'b11: begin
-                                pre_dout <= cache_array[1][127:96];
-                            end
+                        case (block_offset)
+                            2'b00:          pre_dout = cache_array[1][31:0];
+                            2'b01:          pre_dout = cache_array[1][63:32];
+                            2'b10:          pre_dout = cache_array[1][95:64];
+                            2'b11:          pre_dout = cache_array[1][127:96];
                         endcase
                     end
                     else begin
@@ -80,21 +70,13 @@ begin
                     end
                 end
                 2'b10: begin
-                    if ((A[30:6] == cache_array[2][30:6]) && (cache_array[2][155] == 1)) begin
+                    if ((tag == cache_array[2][153:128]) && (cache_array[2][154] == 1)) begin
                         Hit <= 1;
-                        case (A[3:2])
-                            2'b00: begin
-                                pre_dout <= cache_array[2][31:0];
-                            end
-                            2'b01: begin
-                                pre_dout <= cache_array[2][63:32];
-                            end
-                            2'b10: begin
-                                pre_dout <= cache_array[2][95:64];
-                            end
-                            2'b11: begin
-                                pre_dout <= cache_array[2][127:96];
-                            end
+                        case (block_offset)
+                            2'b00:          pre_dout = cache_array[2][31:0];
+                            2'b01:          pre_dout = cache_array[2][63:32];
+                            2'b10:          pre_dout = cache_array[2][95:64];
+                            2'b11:          pre_dout = cache_array[2][127:96];
                         endcase
                     end
                     else begin
@@ -102,21 +84,13 @@ begin
                     end
                 end
                 2'b11: begin
-                    if ((A[30:6] == cache_array[3][30:6]) && (cache_array[3][155] == 1)) begin
+                    if ((tag == cache_array[3][153:128]) && (cache_array[3][154] == 1)) begin
                         Hit <= 1;
-                        case (A[3:2])
-                            2'b00: begin
-                                pre_dout <= cache_array[3][31:0];
-                            end
-                            2'b01: begin
-                                pre_dout <= cache_array[3][63:32];
-                            end
-                            2'b10: begin
-                                pre_dout <= cache_array[3][95:64];
-                            end
-                            2'b11: begin
-                                pre_dout <= cache_array[3][127:96];
-                            end
+                        case (block_offset)
+                            2'b00:          pre_dout = cache_array[3][31:0];
+                            2'b01:          pre_dout = cache_array[3][63:32];
+                            2'b10:          pre_dout = cache_array[3][95:64];
+                            2'b11:          pre_dout = cache_array[3][127:96];
                         endcase
                     end
                     else begin
@@ -124,46 +98,22 @@ begin
                     end
                 end  
         endcase
-        if (ByteAddr) begin
-            case (A[1:0])
-                2'b00: begin
-                    dout_cache <= {(DATA_WIDTH-1:WIDTH)'b0, pre_dout[7:0]};
-                end
-                2'b01: begin
-                    dout_cache <= {(DATA_WIDTH-1:WIDTH)'b0, pre_dout[15:8]};
-                end
-                2'b10: begin
-                    dout_cache <= {(DATA_WIDTH-1:WIDTH)'b0, pre_dout[23:16]};
-                end
-                2'b00: begin
-                    dout_cache <= {(DATA_WIDTH-1:WIDTH)'b0, pre_dout[31:24]};
-                end
-            endcase
-        end
-        else dout_cache <= pre_dout;
+        dout_cache <= ByteAddr ? {24'b0, pre_dout[7:0]} : pre_dout;
     end
 end
 
+logic WEN_cache = ~(Hit);
 
 // writing is synchronous (storing)
 always_ff @(posedge clk) begin
     if (WEN_cache) begin
-        case (A[5:4])
-            2'b00: begin
-                assign cache_array[0] = {1'b1, A[DATA_WIDTH:6], d3, d2, d1, d0};
-            end
-            2'b01: begin
-                assign cache_array[1] = {1'b1, A[DATA_WIDTH:6], d3, d2, d1, d0};
-            end
-            2'b10: begin
-                assign cache_array[2] = {1'b1, A[DATA_WIDTH:6], d3, d2, d1, d0};
-            end
-            2'b11: begin
-                assign cache_array[3] = {1'b1, A[DATA_WIDTH:6], d3, d2, d1, d0};
-            end  
+        case (set_number)
+            2'b00:          cache_array[0] = {1'b1, tag, d3, d2, d1, d0};
+            2'b01:          cache_array[1] = {1'b1, tag, d3, d2, d1, d0};
+            2'b10:          cache_array[2] = {1'b1, tag, d3, d2, d1, d0};
+            2'b11:          cache_array[3] = {1'b1, tag, d3, d2, d1, d0};  
         endcase
     end
 end
-
 
 endmodule
